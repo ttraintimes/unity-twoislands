c4881b7cdc59e0532452c9dc339c16163b30533a
ttraintimes <ttraintimes@gmail.com>
2023-08-08 18:01:00 +0200

    kitty

---
+++ Assets/MenuUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    public void mainBtn()
+    {
+        SceneManager.LoadScene("main");
+    }
+    public void storyBtn2()
+    {
+        SceneManager.LoadScene("paintgame");
+    }
+
+    public void quitBtn()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/ReturnBtn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ReturnBtn : MonoBehaviour
+{
+
+    public void ExitBtn()
+    {
+        int y = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.UnloadSceneAsync(y);
+        foreach (GameObject g in SceneManager.GetSceneByName("main").GetRootGameObjects())
+        {
+            g.SetActive(true);
+        }
+        
+        GameObject.Find("Canvas").SetActive(false);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("main"));
+    }
+
+}
