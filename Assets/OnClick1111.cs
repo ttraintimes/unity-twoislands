3481e4e041407fb1cc32b8b6adff209f7693f740
ttraintimes <ttraintimes@gmail.com>
2023-08-09 13:36:41 +0200

    1

---
+++ Assets/OnClick1111.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick1111: MonoBehaviour {
+  public GameObject Onjs;
+  public GameObject gameobject;
+  void Start() {
+  }
+  void Update() {
+    if (Input.GetMouseButton(0)) {
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit;
+      if (Physics.Raycast(ray, out hit)) {
+        if (hit.collider.tag == "npc3") {
+          Onjs.gameObject.SetActive(true);
+        }
+      }
+    }
+  }
+}
