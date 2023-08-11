e90502b2f693e6e836081503e3df01a0d823dae7
ttraintimes <ttraintimes@gmail.com>
2023-08-11 16:24:10 +0200

    lost child

---
+++ Assets/OnClick11111.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick11111: MonoBehaviour {
+  public GameObject Onjs;
+  public GameObject gameobject;
+  void Start() {
+  }
+  void Update() {
+    if (Input.GetMouseButton(0)) {
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit;
+      if (Physics.Raycast(ray, out hit)) {
+        if (hit.collider.tag == "npc4") {
+          Onjs.gameObject.SetActive(true);
+        }
+      }
+    }
+  }
+}
