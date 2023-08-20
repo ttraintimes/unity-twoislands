8d574f9583c1151a7392bda52927143a3dcefd26
ttraintimes <ttraintimes@gmail.com>
2023-08-20 12:47:34 +0200

    1

--- Assets/GlobalVariables.cs
+++ Assets/GlobalVariables.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class GlobalVariables : MonoBehaviour {
 
 	// Resolution setting
-	public const int SCREEN_WIDTH = 1280;
-	public const int SCREEN_HEIGHT = 600;
+	public const int SCREEN_WIDTH = 1920;
+	public const int SCREEN_HEIGHT = 1080;
 	public const bool isFullScreen = false;
 	public const float text_rolling_speed = 0.018f;
 	public const float fading_speed = 0.5f;
--- Assets/SceneFadeInOut.cs
+++ Assets/SceneFadeInOut.cs
@@ -9,12 +9,12 @@ public class SceneFadeInOut : MonoBehaviour {
 	public Animator animator;
 	public Image black;
 
-	public IEnumerator Fading(string Scene_name)
+	public IEnumerator Fading(string main)
 	{
 		animator.SetInteger ("FadeDir",-1);
 		yield return new WaitUntil (()=>black.color.a >= 0.98f);
 
-		SceneManager.LoadScene (Scene_name);
+		SceneManager.LoadScene (main);
 	}
 		
 }
